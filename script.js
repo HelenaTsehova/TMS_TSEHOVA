a2c064176d79d342cb221bcade5a65f00cb017b3
HelenaTsehova <helnaatsehova@gmail.com>
2022-11-07 18:18:19 +0300

    mobile menu

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
 (function () {
-    const menuIconId = 'burger-icon';
-    const closeIconId = 'header__nav-close';
-    const headerContainerId = 'header';
-
-document.getElementById(menuIconId).addEventListener('click', () => {
-  document.getElementById(headerContainerId).classList.add('mobile-menu');
-});
-
-document.getElementById(closeIconId).addEventListener('click', () => {
-  document.getElementById(headerContainerId).classList.remove('mobile-menu');
-});
+    const burgerItem = document.querySelector('.burger');
+    const menu = document.querySelector('.header_nav');
+    const menuCloseItem = document.querySelector('.header__nav-close');
+    burgerItem.addEventListener('click', () => {
+      menu.classList.add('header_nav-active');
+    })
+    menuCloseItem.addEventListener('click', () => {
+      menu.classList.remove('header_nav-active');
+    })
+}());
